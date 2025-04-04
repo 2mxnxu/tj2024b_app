cb396375aa1f071c0424310bc86a9ef658fe46a1
2ãmxnxu <leeeun099@naver.com>
2025-04-04 13:21:09 +0900

    풀러터

---
+++ lib/example/day02/example4.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp1());
+}
+class MyApp1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(home: Text("안녕하세요 플러터"));
+  }
+}
